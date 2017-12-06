564162f5ce993be604e725404e2ab3e41ef9eaa4
exerceo <alexewert@live.com>
2017-12-06 18:49:38 -0500

    Add system testing

---
+++ src/system_testing/test-script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+data=system_testing/test-data
+file1=system_testing/test
+file2=system_testing/test-output
+
+echo "     *** BEGINNING TEST ***"
+
+cd ..
+make qsolver
+
+cat ${data} | ./qsolver > ${file1}
+
+if cmp -s ${file1} ${file2}; then
+	echo "SUCCESS | The test cases match."
+else
+	echo "FAILURE | The test cases don't match."
+fi
+
+make clean
+rm ${file1}
+
+echo "      *** ENDING TEST ***"
