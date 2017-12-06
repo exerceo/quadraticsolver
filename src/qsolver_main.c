181a3e18c256255f31cb36b62ddaae1d250e02fd
exerceo <alexewert@live.com>
2017-12-06 15:11:19 -0500

    Update error checking and unit tests

--- src/qsolver_main.c
+++ src/qsolver_main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "input/input.h"
 #include "qsolver_math/qsolver.h"
 
@@ -6,21 +8,40 @@ int main(int argc, char* argv[]){
 	int ret;
 	char input[500];
 	double inputs[3];
-
-	printf("Please all 3 numbers on the same line.\n\n");
-	printf("Example:\n2.0 5 -3.0\n\n");
-	read_line(input, 500);
-	printf("\n");
-
-	input_numbers(input, inputs);
-
 	double x1, x2;
 
-	ret = qsolver(inputs[0], inputs[1], inputs[2], &x1, &x2);
+	printf("Please type all 3 numbers on the same line. To exit the program, type 'exit'\n");
+	printf("Example:\n");
+	printf("   > 2.0 5 -3.0\n");
+	printf("   Roots are: x1: 0.500000 and x2: -3.000000\n\n");
 
-	if (ret){
-		printf("The given input is not valid for square roots in the quadratic formula.\nExiting.\n");
-	} else {
-		printf("The two solutions are: %f and %f\n", x1, x2);
+	while (1) {
+		printf("> ");
+		read_line(input, 500);
+
+		if (strcmp(input, "exit\n") == 0)
+			exit(1);
+
+		input_numbers(input, inputs);
+		ret = qsolver(inputs[0], inputs[1], inputs[2], &x1, &x2);
+
+		switch(ret) {
+			case -2:
+				printf("No real roots\n\n");
+				break;
+			case -1:
+				printf("Not a quadratic equation (a == 0)\n\n");
+				break;
+			case 0:
+				printf("Roots are: x1: %f and x2: %f\n\n", x1, x2);
+				break;
+			case 1:
+				printf("Double real root: %f\n\n", x1);
+				break;
+			default:
+				fprintf(stderr,"System failure: qsolver error code=%d", ret);
+				exit(1);
+				break;
+		}
 	}
 }
--- src/qsolver_math/qsolver.c
+++ src/qsolver_math/qsolver.c
@@ -9,20 +9,23 @@
 int qsolver(double a, double b, double c, double *x1, double *x2) {
 	double disc, sqrt_disc;
 
-	if (a == 0.0) {
-		return 3;
+	if (a == 0.0) { /* not a quadratic */
+		return -1;
 	}
 
 	disc = b*b-4.0*a*c;
 
-	if (disc < 0.0) {
-		return 2;
-	} else if (disc == 0.0) {
+	if (disc < 0.0) { /* no real roots */
+		return -2;
+	} else if (disc == 0.0) { /* double root */
+		*x1 = -b / (2.0*a);
+		*x2 = *x1;
 		return 1;
 	}
 
 	sqrt_disc = sqrt(disc);
 
+	/* two roots */
 	*x1 = ((-b)+sqrt_disc)/(2*a);
 	*x2 = ((-b)-sqrt_disc)/(2*a);
 
