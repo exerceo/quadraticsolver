8579436e6551088221c475f32a31f4d0a067c3f1
exerceo <alexewert@live.com>
2017-12-07 22:08:37 -0500

    Add more unit tests

--- src/cunit/t2.c
+++ src/cunit/t2.c
@@ -6,21 +6,66 @@
 int main() {
 	char *input = malloc(sizeof(char) * 256);
 	double inputs[3];
+	double in1, in2, in3;
 	int ret;
 	cunit_init();
 
 	// good unit test; should return 0
 	input = "1 2 3";
+	in1 = 1.0;
+	in2 = 2.0;
+	in3 = 3.0;
 	ret = input_numbers(input, inputs) ;
 	assert_eq("ret", ret, 0);
+	assert_feqrerr("inputs[0]", inputs[0], in1, 10.0*cunit_dmacheps );
+	assert_feqrerr("inputs[1]", inputs[1], in2, 10.0*cunit_dmacheps );
+	assert_feqrerr("inputs[2]", inputs[2], in3, 10.0*cunit_dmacheps );
 
 	// good unit test; should return 0
 	input = "3 2 1";
+	in1 = 3.0;
+	in2 = 2.0;
+	in3 = 1.0;
 	ret = input_numbers(input, inputs) ;
 	assert_eq("ret", ret, 0);
+	assert_feqrerr("inputs[0]", inputs[0], in1, 10.0*cunit_dmacheps );
+	assert_feqrerr("inputs[1]", inputs[1], in2, 10.0*cunit_dmacheps );
+	assert_feqrerr("inputs[2]", inputs[2], in3, 10.0*cunit_dmacheps );
+
+	// good unit test; should return 0
+	input = "3.5 21 1.21";
+	in1 = 3.5;
+	in2 = 21.0;
+	in3 = 1.21;
+	ret = input_numbers(input, inputs) ;
+	assert_eq("ret", ret, 0);
+	assert_feqrerr("inputs[0]", inputs[0], in1, 10.0*cunit_dmacheps );
+	assert_feqrerr("inputs[1]", inputs[1], in2, 10.0*cunit_dmacheps );
+	assert_feqrerr("inputs[2]", inputs[2], in3, 10.0*cunit_dmacheps );
+
+	// good unit test; should return 0
+	input = "8.5123 13321.1232323 1555.21123154151325";
+	in1 = 8.5123;
+	in2 = 13321.1232323;
+	in3 = 1555.21123154151325;
+	ret = input_numbers(input, inputs) ;
+	assert_eq("ret", ret, 0);
+	assert_feqrerr("inputs[0]", inputs[0], in1, 10.0*cunit_dmacheps );
+	assert_feqrerr("inputs[1]", inputs[1], in2, 10.0*cunit_dmacheps );
+	assert_feqrerr("inputs[2]", inputs[2], in3, 10.0*cunit_dmacheps );
+
+	// good unit test; should return -1
+	input = "";
+	ret = input_numbers(input, inputs) ;
+	assert_eq("ret", ret, -1);
 
 	// good unit test; should return -1
 	input = "1 2";
+	ret = input_numbers(input, inputs) ;
+	assert_eq("ret", ret, -1);
+
+	// good unit test; should return -1
+	input = "5 9 3 a";
 	ret = input_numbers(input, inputs) ;
 	assert_eq("ret", ret, -1);
 
